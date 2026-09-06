ad2092827a854237bf55e108a2ad4ef402bee2df
hwwgo <62611942+hwwgo@users.noreply.github.com>
2026-09-06 12:36:12 +0800

    Update MainActivity.kt

--- app/src/main/java/cn/huacheng/safebaiyun/MainActivity.kt
+++ app/src/main/java/cn/huacheng/safebaiyun/MainActivity.kt
@@ -18,11 +18,15 @@ import cn.huacheng.safebaiyun.compose.MainView
 import cn.huacheng.safebaiyun.compose.QRExportView
 import cn.huacheng.safebaiyun.compose.QRImportView
 import cn.huacheng.safebaiyun.theme.SafeBaiyunTheme
-
+import cn.huacheng.safebaiyun.unlock.UnlockRepo
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // ========== 新增：传入 lifecycleScope 给 UnlockRepo ==========
+        UnlockRepo.init(lifecycleScope)
+
         setContent {
             SafeBaiyunTheme {
                 val navController = rememberNavController()
