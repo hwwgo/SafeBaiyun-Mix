891c282f2c718890813d17d9ee8d3181b213ced9
hwwgo <62611942+hwwgo@users.noreply.github.com>
2026-09-06 20:05:50 +0800

    Update SettingsView.kt

--- app/src/main/java/cn/huacheng/safebaiyun/compose/SettingsView.kt
+++ app/src/main/java/cn/huacheng/safebaiyun/compose/SettingsView.kt
@@ -6,37 +6,37 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compute.foundation.layout.height
+import androidx.compute.foundation.layout.padding
+import androidx.compute.foundation.layout.width
+import androidx.compute.foundation.rememberScrollState
+import androidx.compute.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Restore
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compute.material.icons.filled.ArrowBack
+import androidx.compute.material.icons.filled.Restore
+import androidx.compute.material3.Button
+import androidx.compute.material3.Card
+import androidx.compute.material3.CardDefaults
+import androidx.compute.material3.ExperimentalMaterial3Api
+import androidx.compute.material3.Icon
+import androidx.compute.material3.IconButton
+import androidx.compute.material3.MaterialTheme
+import androidx.compute.material3.OutlinedTextField
+import androidx.compute.material3.Scaffold
+import androidx.compute.material3.Text
+import androidx.compute.material3.TopAppBar
+import androidx.compute.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compute.runtime.getValue
+import androidx.compute.runtime.mutableStateOf
+import androidx.compute.runtime.remember
+import androidx.compute.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compute.ui.text.font.FontWeight
+import androidx.compute.ui.unit.dp
+import androidx.compute.ui.unit.sp
 import androidx.navigation.NavController
 import cn.huacheng.safebaiyun.util.ConfigManager
 import cn.huacheng.safebaiyun.util.showToast
@@ -95,7 +95,7 @@ fun SettingsView(navController: NavController) {
                     }
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
                 )
             )
         }
