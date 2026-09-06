fbb31b7e2e014aa3b6fcc840c411c4d0841590e7
hwwgo <62611942+hwwgo@users.noreply.github.com>
2026-09-06 22:51:29 +0800

    Update MainView.kt

--- app/src/main/java/cn/huacheng/safebaiyun/compose/MainView.kt
+++ app/src/main/java/cn/huacheng/safebaiyun/compose/MainView.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
