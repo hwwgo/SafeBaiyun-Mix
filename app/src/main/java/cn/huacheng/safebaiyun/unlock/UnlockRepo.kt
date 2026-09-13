a7e501f1dc946388d8b7942a84524d19020ed17f
hwwgo <62611942+hwwgo@users.noreply.github.com>
2026-09-13 16:14:56 +0800

    Update UnlockRepo.kt

--- app/src/main/java/cn/huacheng/safebaiyun/unlock/UnlockRepo.kt
+++ app/src/main/java/cn/huacheng/safebaiyun/unlock/UnlockRepo.kt
@@ -22,6 +22,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
