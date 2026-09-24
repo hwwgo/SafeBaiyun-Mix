ae7b3c531940b71864b4bbc76731417834093f2e
hwwgo <62611942+hwwgo@users.noreply.github.com>
2026-09-25 01:08:19 +0800

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.material.icons.core)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.glance)
     implementation(libs.navigation)
